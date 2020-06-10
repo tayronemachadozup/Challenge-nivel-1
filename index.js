452a36bf88bf828b11fce7f8392f20d449217391
tayronemachadozup <tayrone.machado@zup.com.br>
2020-06-10 12:28:47 -0300

    add change of carousel

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const carouselElement = document.querySelector('#carousel');
+const carouselElement = document.querySelector('#slideshow');
 const IMG_URL = 'https://image.tmdb.org/t/p/w533_and_h300_bestv2/';
 
 function createCarousel(arrMovies){
     const carousel = `
+    <ul class="carousel__slideshow">
         ${arrMovies.map(movie =>  `
-        <img class ="carousel__img" src="${IMG_URL + movie.poster_path}">
-        `).join('')}    
-    `
+        <li class="carousel__slideshow__img"><img class ="carousel__img" src="${IMG_URL + movie.poster_path}"></li>
+        `).join('')}
+    </ul>        
+    `;
     carouselElement.innerHTML = carousel;
 }
 
